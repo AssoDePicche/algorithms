5ed363a2323f9bf6fe4e5ccfa9f7f31ee339483f
AssoDePicche <samuelprado730@gmail.com>
2023-02-20 00:11:51 -0300

    perf: update while loop to a recursive call

--- src/search/binarysearch/BinarySearch.java
+++ src/search/binarysearch/BinarySearch.java
@@ -1,23 +1,21 @@
 public class BinarySearch {
-  public static int search(int[] list, int target) {
-    int bottom = 0, top = list.length;
-
-    while (bottom < top) {
-      int middle = (int) Math.floor((bottom + top) / 2);
-
-      int guess = list[middle];
-
-      if (guess == target) {
-        return middle;
-      }
-
-      if (guess > target) {
-        top = middle - 1;
-      } else {
-        bottom = middle + 1;
-      }
+  public static int search(int[] list, int start, int end, int target) {
+    if (start > end) {
+      return -1;
     }
 
-    return -1;
+    int middle = (start + end) / 2;
+
+    int guess = list[middle];
+
+    if (guess == target) {
+      return middle;
+    }
+
+    if (guess > target) {
+      return search(list, start, middle - 1, target);
+    }
+
+    return search(list, middle + 1, end, target);
   }
 }
