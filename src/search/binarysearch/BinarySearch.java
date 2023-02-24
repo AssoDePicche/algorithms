85e13bf2ae7f64ec25eb370a87a204823c6a4771
AssoDePicche <samuelprado730@gmail.com>
2023-02-24 16:59:27 -0300

    refactor: improve the implementation of binary search in java using hash code

--- src/search/binarysearch/BinarySearch.java
+++ src/search/binarysearch/BinarySearch.java
@@ -1,21 +1,25 @@
-public class BinarySearch<T extends Number> {
+public class BinarySearch<T> {
   public int search(T[] list, int start, int end, T target) {
-    if (start > end) {
+    try {
+      if (start > end) {
+        return -1;
+      }
+
+      int middle = (start + end) / 2;
+
+      T guess = list[middle];
+
+      if (guess.equals(target)) {
+        return middle;
+      }
+
+      if (guess.hashCode() > target.hashCode()) {
+        return search(list, start, middle - 1, target);
+      }
+
+      return search(list, middle + 1, end, target);
+    } catch (ArrayIndexOutOfBoundsException exception) {
       return -1;
     }
-
-    int middle = (start + end) / 2;
-
-    T guess = list[middle];
-
-    if (guess.equals(target)) {
-      return middle;
-    }
-
-    if (guess.doubleValue() > target.doubleValue()) {
-      return search(list, start, middle - 1, target);
-    }
-
-    return search(list, middle + 1, end, target);
   }
 }
